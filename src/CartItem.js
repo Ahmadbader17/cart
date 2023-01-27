99349493800b2ef44e52c99deab31af6f8e1eccb
Ahmad Bader <56409106+Ahmadbader17@users.noreply.github.com>
2023-01-27 15:30:59 +0530

    resolving issues related to firebase

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,109 +1,45 @@
 import React from 'react';
 
 class CartItem extends React.Component {
-  constructor () {
-    super();
-    this.state = {
-      price: 999,
-      title: 'Mobile Phone',
-      qty: 1,
-      img: ''
-    }
-    // this.increaseQuantity = this.increaseQuantity.bind(this);
-    // this.testing();
-  }
-
-  // testing () {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve('done');
-  //     }, 5000);
-  //   })
-
-  //   promise.then(() => {
-  //     // setState acts like a synchronus call
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     this.setState({ qty: this.state.qty + 10 });
-
-  //     console.log('state', this.state);
-  //   });
-  // }
-  increaseQuantity = () => {
-    // this.state.qty += 1;
-    // console.log('this', this.state);
-    // setState form 1
-    // this.setState({
-    //   qty: this.state.qty + 1
-    // }, () => {});
-
-    // setState form 2 - if prevState required use this
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty + 1
-      }
-    });
-  }
-
-  decreaseQuantity = () => {
-    const { qty } = this.state;
-
-    if (qty === 0) {
-      return;
-    }
-    // setState form 2 - if prevState required use this
-    this.setState((prevState) => {
-      return {
-        qty: prevState.qty - 1
-      }
-    });
-  }
   render () {
-    console.log('render');
-    const { price, title, qty } = this.state;
+    const { qty, price, title } = this.props.product;
+    const { product } = this.props;
+
     return (
       <div className="cart-item">
         <div className="left-block">
-          <img style={styles.image} />
+          <img
+            style={{ height: 110, width: 110, borderRadius: 5, background: '#ccc' }}
+            src={product.img}
+          />
         </div>
         <div className="right-block">
-          <div style={ { fontSize: 25 } }>{title}</div>
-          <div style={ { color: '#777' } }>Rs {price} </div>
-          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div style={{ fontSize: 25 }}> {title} </div>
+          <div style={{ color: '#777' }}>{price}</div>
+          <div style={{ color: '#777' }}>Qty: {qty}</div>
           <div className="cart-item-actions">
-            {/* Buttons */}
-            <img
-              alt="increase"
+            <img 
+              alt="add"
+              src="https://image.flaticon.com/icons/svg/1665/1665578.svg"
               className="action-icons"
-              src="https://image.flaticon.com/icons/svg/992/992651.svg"
-              onClick={this.increaseQuantity}
+              onClick={() => this.props.onIncreaseQuantity(product)}
             />
             <img
-              alt="decrease"
-              className="action-icons"
+              alt="minus"
               src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
-              onClick={this.decreaseQuantity}
+              className="action-icons"
+              onClick={() => this.props.onDecreaseQuantity(product)}
             />
             <img
               alt="delete"
-              className="action-icons"
               src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+              className="action-icons"
+              onClick={() => this.props.onDeleteProduct(product.id)}
             />
           </div>
         </div>
       </div>
-    );
-  }
-}
-
-const styles = {
-  image: {
-    height: 110,
-    width: 110,
-    borderRadius: 4,
-    background: '#ccc'
+    )
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import * as firebase from "firebase";
+import "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAiT-FS6eXz3-v4vTISv4A7fBvpAy68XcA",
+    authDomain: "cart-aa83e.firebaseapp.com",
+    projectId: "cart-aa83e",
+    storageBucket: "cart-aa83e.appspot.com",
+    messagingSenderId: "262543966832",
+    appId: "1:262543966832:web:ca5cfb31a6fd33a0a7aab5"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
